69d42c89bd838230dfcaf7ef78dd426c9f3691df
chenziwen <chenziwen@qesong.com>
2024-04-24 10:16:08 +0800

    remove: some file

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 #!/usr/bin/env node
 
-const { createCommander } = require("./workers/app");
+const commander = require("commander");
+const program = new commander.Command();
+
+program.version("0.0.1", '-v, --version', 'outout the current version');
+
+program
+    .option("-i, --input <char>", "Input language", "en")
+    .option("-o, --output <char>", "Output language", "zh-Hans")
+// .option("");
+// .option()
+
+program.parse(process.argv); // 执行这个函数后 通过shell输入的指令参数才会被解析到options中
+
+// 自定义选项处理 原理就跟reduce方法一个意思
+const options = program.opts();
+console.log(options);
 
 
-createCommander();
+
