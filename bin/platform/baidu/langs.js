f9c6b0b592db8872107c06d0d2a69e38a8d5a838
chenziwen <chenziwen@qesong.com>
2024-06-26 18:02:44 +0800

    add: language template

--- bin/platform/baidu/langs.js
+++ bin/platform/baidu/langs.js
@@ -1,6 +1,68 @@
 const langs = {
-    excludeLang: [], // 全部语言中排除的语言
-    
+    "auto": {
+        code: "",
+        exclude: []
+    },
+    "chinese": {
+        code: "",
+        exclude: []
+    },
+    "russian": {
+        coe: "",
+        exclude: []
+    },
+    "english": {
+        code: "",
+        exclude: []
+    },
+    "japanese": {
+        code: "",
+        exclude: []
+    },
+    "korean": {
+        code: "",
+        exclude: []
+    },
+    "french": {
+        code: "",
+        exclude: []
+    },
+    "spanish": {
+        code: "",
+        exclude: []
+    },
+    "thai": {
+        code: "",
+        exclude: []
+    },
+    "portuguese": {
+        code: "",
+        exclude: []
+    },
+    "german": {
+        code: "",
+        exclude: []
+    },
+    "italian": {
+        code: "",
+        exclude: []
+    },
+    "traditional_chinese": {
+        code: "",
+        exclude: []
+    },
+    "vietnamese": {
+        code: "",
+        exclude: []
+    },
+    "dutch": {
+        code: "",
+        exclude: []
+    },
+    "polish": {
+        code: "",
+        exclude: []
+    }
 }
 
 module.exports = langs; 
