4c9e1be76d488d8f5505c9c629b7ad7e9cad1ba6
chenziwen <chenziwen@qesong.com>
2024-07-11 14:30:20 +0800

    add: platform language judge

--- bin/platform/ali/langs.js
+++ bin/platform/ali/langs.js
@@ -2,23 +2,75 @@ const langs = {
     codeMapping: {
         auto: "auto",
         zh: "zh",
-        cht: "cht",
+        cht: "zh-tw",
         en: "en",
         ru: "ru",
-        ja: "jp",
-        ko: "kor",
+        ja: "ja",
+        ko: "ko",
         de: "de",
-        fr: "fra",
-        es: "spa",
+        fr: "fr",
+        es: "es",
         it: "it",
         pt: "pt",
         th: "th"
     },
     source: {
-
+        strategy: "exclude",
+        language: []
     },
     target: {
-
+        auto: {
+            strategy: "exclude",
+            language: ["auto"]
+        },
+        zh: {
+            strategy: "exclude",
+            language: ["auto"],
+        },
+        cht: {
+            strategy: "include",
+            language: ["zh"],
+        },
+        en: {
+            strategy: "exclude",
+            language: ["auto"],
+        },
+        ru: {
+            strategy: "exclude",
+            language: ["auto"],
+        },
+        ja: {
+            strategy: "exclude",
+            language: ["auto"],
+        },
+        ko: {
+            strategy: "exclude",
+            language: ["auto"],
+        },
+        de: {
+            strategy: "exclude",
+            language: ["auto"],
+        },
+        fr: {
+            strategy: "exclude",
+            language: ["auto"],
+        },
+        es: {
+            strategy: "exclude",
+            language: ["auto"],
+        },
+        it: {
+            strategy: "exclude",
+            language: ["auto"],
+        },
+        pt: {
+            strategy: "exclude",
+            language: ["auto"],
+        },
+        th: {
+            strategy: "exclude",
+            language: ["auto"],
+        }
     }
 }
 
