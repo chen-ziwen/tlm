edb669ef77675275d9099e960d23e1939ad76d83
Chiko <2452559902@qq.com>
2024-07-21 21:10:26 +0800

    fixed: gitignore dist/

--- dist/bin/platform/langs.js
+++
@@ -1,6 +0,0 @@
-export { default as baidu } from "./baidu/langs.js";
-export { default as google } from "./google/langs.js";
-export { default as youdao } from "./youdao/langs.js";
-export { default as tencent } from "./tencent/langs.js";
-export { default as ali } from "./ali/langs.js";
-export { default as volcengine } from "./volcengine/langs.js";
