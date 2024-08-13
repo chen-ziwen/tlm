bb8387513a006fe31d5e143cb1489ed17a3a0aa9
Chiko <2452559902@qq.com>
2024-08-14 00:04:30 +0800

    upd: publish

--- src/util/updated_config.ts
+++ src/util/updated_config.ts
@@ -3,13 +3,15 @@ import { MPTLRC, CONFIG_PATH } from "@/constants";
 
 // npm install 或者 npm update 更新配置文件
 (async () => {
-    const config = <Tl.Config>await readFile(CONFIG_PATH);
-    const isExist = await isExistConfig(MPTLRC);
-    if (!isExist) {
-        await writeFile(MPTLRC, config);
-    } else {
-        const localConfig = await readFile(MPTLRC);
-        Object.assign(config, localConfig);
-        await writeFile(MPTLRC, config);
+    if (process.env.SKIP_POSTINSTALL !== "true") {
+        const config = <Tl.Config>await readFile(CONFIG_PATH);
+        const isExist = await isExistConfig(MPTLRC);
+        if (!isExist) {
+            await writeFile(MPTLRC, config);
+        } else {
+            const localConfig = await readFile(MPTLRC);
+            Object.assign(config, localConfig);
+            await writeFile(MPTLRC, config);
+        }
     }
 })();
