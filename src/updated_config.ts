73e2e92ab40fdfc43e9799f4d5b5e6c6d90c7dd9
chenziwen <chenziwen@qesong.com>
2024-08-14 09:00:44 +0800

    opt: git action

--- src/updated_config.ts
+++ src/updated_config.ts
@@ -4,7 +4,7 @@ import { MPTLRC, CONFIG_PATH } from "@/constants";
 // npm install 或者 npm update 更新配置文件
 (async () => {
     if (process.env.SKIP_POSTINSTALL !== "true") {
-        const config = await readFile(CONFIG_PATH);
+        const config = <Tl.Config>await readFile(CONFIG_PATH);
         const isExist = await isExistConfig(MPTLRC);
         if (!isExist) {
             await writeFile(MPTLRC, config);
